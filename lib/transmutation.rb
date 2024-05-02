318cc0a3de0a1e6eaa23b8405cfea9e8942c70cd
borrabeer <borrabeer@outlook.com>
2024-05-02 21:18:42 +0700

    Add Serialization and CollectionSerializer

--- lib/transmutation.rb
+++ lib/transmutation.rb
@@ -2,6 +2,8 @@
 
 require_relative "transmutation/version"
 require_relative "transmutation/serializer"
+require_relative "transmutation/serialization"
+require_relative "transmutation/collection_serializer"
 
 module Transmutation
   class Error < StandardError; end
---
+++ lib/transmutation/serialization.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Transmutation
+  module Serialization # rubocop:disable Style/Documentation
+    def self.lookup_serializer(object)
+      "#{object.class}Serializer".constantize
+    end
+
+    def render(**args)
+      return super(**args) unless args[:json]
+      return super(**args) if args[:serialize] == false
+
+      return super(**args, json: Transmutation::CollectionSerializer.new(args[:json])) if args[:json].respond_to?(:map)
+
+      super(**args, json: Transmutation::Serialization.lookup_serializer(args[:json]).new(args[:json]))
+    end
+  end
+end
