12b34be22acb1177070ed0bf615e63c6ad2a3217
borrabeer <borrabeer@outlook.com>
2024-05-09 15:10:11 +0700

    Add tests to Serialization

--- spec/transmutation/serialization_spec.rb
+++ spec/transmutation/serialization_spec.rb
@@ -38,28 +38,47 @@ RSpec.describe Transmutation::Serialization do
                                                                            last_name: "Doe" } })
     end
 
-    it "calls super when :serialize is false" do # rubocop:disable RSpec/MultipleExpectations
+    it "calls super without serializer when :serialize is false" do
       json = controller.render(json: example_object.to_h, serialize: false)
       expect(json.keys).to contain_exactly(:first_name, :last_name)
-      expect(json[:first_name]).to eq("John")
-      expect(json[:last_name]).to eq("Doe")
     end
 
-    it "calls super with Transmutation::CollectionSerializer when :json responds to :map" do # rubocop:disable RSpec/ExampleLength,RSpec/MultipleExpectations
-      example_array = [example_object]
-      json = controller.render(json: example_array)
+    describe "calls super with Transmutation::CollectionSerializer when :json responds to :map" do
+      subject(:json) { controller.render(json: example_array) }
 
-      expect(json).to be_an(Array)
-      expect(json.length).to eq(1)
+      let(:example_array) { [example_object] }
 
-      serialized_object = json.first
-      expect(serialized_object).to be_a(Hash)
-      expect(serialized_object.keys).to contain_exactly("first_name")
-      expect(serialized_object).to eq({ "first_name" => "John" })
+      it "returns an array" do
+        expect(json).to be_an(Array)
+      end
+
+      it "returns an array with serialized objects" do
+        expect(json.first).to be_a(Hash)
+      end
+
+      it "returns an array with serialized objects with keys defined" do
+        expect(json.first.keys).to contain_exactly("first_name")
+      end
+
+      it "returns a serialized array" do
+        expect(json.first).to eq({ "first_name" => "John" })
+      end
     end
 
     it "calls super with the serializer for :json when :json does not respond to :map" do
       expect(controller.render(json: example_object)).to eq({ "first_name" => "John" })
+    end
+  end
+
+  describe "#lookup_serializer" do
+    it "returns the serializer class for a given object" do
+      serializer_class = described_class.lookup_serializer(example_object)
+      expect(serializer_class).to eq(ExampleObjectSerializer)
+    end
+
+    it "raises an error if the serializer class is not found" do
+      object = Struct.new(:first_name, :last_name).new("John", "Doe")
+      expect { described_class.lookup_serializer(object) }.to raise_error(NameError)
     end
   end
 end
