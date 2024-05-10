d78a0ddbc2bcc9a47b0721dbb87a594ccc247ded
borrabeer <borrabeer@outlook.com>
2024-05-10 15:41:48 +0700

    Add serialize method to Serialization

--- lib/transmutation/serialization.rb
+++ lib/transmutation/serialization.rb
@@ -6,13 +6,17 @@ module Transmutation
       Object.const_get("::#{object.class}Serializer")
     end
 
+    def self.serialize(object, options = {})
+      lookup_serializer(object).new(object).as_json(options)
+    end
+
     def render(**args)
       return super(**args) unless args[:json]
       return super(**args) if args[:serialize] == false
 
       return super(**args, json: Transmutation::CollectionSerializer.new(args[:json])) if args[:json].respond_to?(:map)
 
-      super(**args, json: Transmutation::Serialization.lookup_serializer(args[:json]).new(args[:json]))
+      super(**args, json: Transmutation::Serialization.serialize(args[:json]))
     end
   end
 end
