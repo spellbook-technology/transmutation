537c11b3f63cb67cd29f50ec0048a0d40ec6a709
borrabeer <borrabeer@outlook.com>
2024-05-10 17:25:06 +0700

    Update lookup_serializer to accept a namespace argument

--- lib/transmutation/serialization.rb
+++ lib/transmutation/serialization.rb
@@ -1,15 +1,27 @@
 # frozen_string_literal: true
 
+require_relative "utils"
+
 module Transmutation
   module Serialization # rubocop:disable Style/Documentation
-    def self.lookup_serializer(object)
-      lookup_serializer!(object)
+    def self.lookup_serializer(object, namespace: "")
+      lookup_serializer!(object, namespace)
     rescue NameError
       nil
     end
 
-    def self.lookup_serializer!(object)
-      Object.const_get("::#{object.class}Serializer")
+    def self.lookup_serializer!(object, namespace: "")
+      return Object.const_get("::#{object.class}Serializer") if namespace.empty?
+
+      class_name = object.class.name
+      parent_module = class_name.include?("::") ? class_name.split("::").first : ""
+      object_class = class_name.include?("::") ? class_name.split("::").last : object.class
+
+      namespace = Transmutation::Utils.to_pascal_case(namespace)
+
+      return Object.const_get("#{namespace}::#{object_class}Serializer") if namespace.include?("::")
+
+      Object.const_get("#{parent_module}::#{namespace}::#{object_class}Serializer")
     end
 
     def self.serialize(object)
