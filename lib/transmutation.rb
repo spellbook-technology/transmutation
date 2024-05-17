7b7f73e107dfd3318a57785c940d2292b5a5fc45
borrabeer <borrabeer@outlook.com>
2024-05-17 20:46:28 +0700

    Update Serialization to store the caller class to support namespace argument

--- lib/transmutation.rb
+++ lib/transmutation.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "transmutation/version"
+require_relative "transmutation/string_refinements"
 require_relative "transmutation/serializer"
 require_relative "transmutation/serialization"
 require_relative "transmutation/collection_serializer"
--- lib/transmutation/serialization.rb
+++ lib/transmutation/serialization.rb
@@ -1,40 +1,71 @@
 # frozen_string_literal: true
 
-require_relative "utils"
-
-module Transmutation
+module Transmutation # rubocop:disable Style/Documentation
+  using Transmutation::StringRefinements
   module Serialization # rubocop:disable Style/Documentation
-    def self.lookup_serializer(object, namespace: "")
-      lookup_serializer!(object, namespace: namespace)
-    rescue NameError
-      nil
-    end
-
-    def self.lookup_serializer!(object, namespace: "")
-      return Object.const_get("::#{object.class}Serializer") if namespace.empty?
-
-      class_name = object.class.name
-      parent_module = class_name.include?("::") ? class_name.split("::").first : ""
-      object_class = class_name.include?("::") ? class_name.split("::").last : object.class
-
-      namespace = Transmutation::Utils.to_pascal_case(namespace)
-
-      return Object.const_get("#{namespace}::#{object_class}Serializer") if namespace.include?("::")
-
-      Object.const_get("#{parent_module}::#{namespace}::#{object_class}Serializer")
-    end
-
-    def self.serialize(object, namespace: "")
-      lookup_serializer!(object, namespace: namespace).new(object)
+    def self.included(base)
+      base.extend(ClassMethods)
     end
 
     def render(**args)
       return super(**args) unless args[:json]
       return super(**args) if args[:serialize] == false
 
-      return super(**args, json: Transmutation::CollectionSerializer.new(args[:json])) if args[:json].respond_to?(:map)
+      if args[:json].respond_to?(:map)
+        return super(**args, json: Transmutation::CollectionSerializer.new(args[:json],
+                                                                           self.class))
+      end
 
-      super(**args, json: Transmutation::Serialization.serialize(args[:json]))
+      super(**args, json: self.class.serialize(args[:json]))
+    end
+
+    def self.lookup_serializer!(*args, **kwargs)
+      ClassMethods.lookup_serializer!(*args, **kwargs)
+    end
+
+    def self.lookup_serializer(*args, **kwargs)
+      ClassMethods.lookup_serializer(*args, **kwargs)
+    end
+
+    def self.serialize(object, **kwargs)
+      ClassMethods.serialize(object, **kwargs)
+    end
+
+    module ClassMethods # rubocop:disable Style/Documentation
+      def self.extended(base)
+        caller_class = (base.instance_of?(Class) ? base.to_s : self.class.to_s)
+        caller_namespace = caller_class.include?("::") ? caller_class.split("::").first : ""
+        base.instance_variable_set(:@caller_namespace, caller_namespace)
+      end
+
+      def lookup_serializer(object, namespace: "")
+        lookup_serializer!(object, namespace: namespace)
+      rescue NameError
+        nil
+      end
+
+      def lookup_serializer!(object, namespace: "")
+        caller_namespace = instance_variable_get(:@caller_namespace)
+        puts caller_namespace, object.class, namespace
+
+        return Object.const_get("#{caller_namespace}::#{object.class}Serializer") if namespace.empty?
+
+        converted_namespace = namespace.to_s.camelcase
+
+        return Object.const_get("#{converted_namespace}::#{object.class}Serializer") if namespace.to_s.include?("::")
+
+        Object.const_get("#{caller_namespace}::#{converted_namespace}::#{object.class}Serializer")
+      end
+
+      def serialize(object, namespace: "")
+        lookup_serializer!(object, namespace: namespace).new(object)
+      end
+
+      private
+
+      def caller_namespace
+        instance_variable_get(:@caller_namespace)
+      end
     end
   end
 end
