ade43951fdf5e6ee05462972d431369f7ea8fa16
borrabeer <borrabeer@outlook.com>
2024-05-24 19:30:41 +0700

    Refactor Serialization to accept a variant argument

--- lib/transmutation/collection_serializer.rb
+++ lib/transmutation/collection_serializer.rb
@@ -4,13 +4,14 @@ module Transmutation
   class CollectionSerializer # rubocop:disable Style/Documentation
     include Transmutation::Serialization
 
-    def initialize(objects, namespace: "")
+    def initialize(objects, namespace: "", variant: nil)
       @objects = objects
       @namespace = namespace
+      @variant = variant
     end
 
     def as_json(options = {})
-      serializers = serialize(objects, namespace: namespace)
+      serializers = serialize(objects, namespace: namespace, variant: variant)
 
       serializers.map do |serializer|
         serializer.as_json(options)
@@ -19,6 +20,6 @@ module Transmutation
 
     private
 
-    attr_reader :objects, :namespace
+    attr_reader :objects, :namespace, :variant
   end
 end
