d345216f0e2a6d118fc16c63ecb1c68c128fce46
Daniel Dye <dnitemaeric@gmail.com>
2024-05-30 11:51:26 +0100

    Add an empty line between "using" and "module"
    Helps break up the code a little bit.

--- lib/transmutation/serialization.rb
+++ lib/transmutation/serialization.rb
@@ -2,6 +2,7 @@
 
 module Transmutation # rubocop:disable Style/Documentation
   using Transmutation::StringRefinements
+
   module Serialization # rubocop:disable Style/Documentation
     def self.included(base)
       caller_class = (base.instance_of?(Class) ? base.to_s : self.class.to_s)
