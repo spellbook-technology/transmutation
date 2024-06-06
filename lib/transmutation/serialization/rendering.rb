35c780b7337b705637eef698af1efae424c4ca51
Daniel Dye <dnitemaeric@gmail.com>
2024-06-06 23:02:27 +0100

    Add support for associations in serializers
    Associations in serializers lean on the `#serialize` method defined in the `Transmutation::Serialization` module.
    
    This means that associations defined in a serializer under the `Api::V1` namespace will also attempt to be found within the `Api::V1` namespace.

--- lib/transmutation/serialization/rendering.rb
+++ lib/transmutation/serialization/rendering.rb
@@ -3,11 +3,11 @@
 module Transmutation
   module Serialization
     module Rendering
-      def render(json: nil, serialize: true, namespace: nil, serializer: nil, **args)
+      def render(json: nil, serialize: true, namespace: nil, serializer: nil, max_depth: 1, **args)
         return super(**args) unless json
         return super(**args, json: json) unless serialize
 
-        super(**args, json: serialize(json, namespace: namespace, serializer: serializer))
+        super(**args, json: serialize(json, namespace: namespace, serializer: serializer, max_depth: max_depth))
       end
     end
   end
