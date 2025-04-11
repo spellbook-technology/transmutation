924962023f7d50427a21b9a7a7eeaddaf9739e35
Daniel Dye <dnitemaeric@gmail.com>
2025-04-11 22:36:46 +0100

    Add ability to pass block to association
    The return value of the block passed to the `association` call is automatically serialized.

--- spec/system/dummy/serializers/api/v1/detailed/user_serializer.rb
+++ spec/system/dummy/serializers/api/v1/detailed/user_serializer.rb
@@ -7,6 +7,10 @@ module Api
         attributes :first_name, :last_name
 
         has_many :posts, namespace: "::Api::V1"
+
+        has_many :published_posts, namespace: "::Api::V1" do
+          object.posts.reject { |post| post.published_at.nil? }
+        end
       end
     end
   end
