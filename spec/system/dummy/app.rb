ced80ab3e0b6f0ca4f16ede2952228a747e5ceb6
Daniel Dye <dnitemaeric@gmail.com>
2025-04-18 17:19:48 +0100

    Use Zeitwerk for dummy app

--- spec/system/dummy/app.rb
+++ spec/system/dummy/app.rb
@@ -1,20 +1,10 @@
 # frozen_string_literal: true
 
 require "json"
+require "zeitwerk"
 
-require_relative "controllers/base_controller"
-require_relative "controllers/api/application_controller"
-require_relative "controllers/api/v1/health_controller"
-require_relative "controllers/api/v1/posts_controller"
-require_relative "controllers/api/v1/products_controller"
-require_relative "controllers/api/v1/users_controller"
-require_relative "lib/money"
-require_relative "models/post"
-require_relative "models/product"
-require_relative "models/user"
-require_relative "serializers/api/v1/post_serializer"
-require_relative "serializers/api/v1/user_serializer"
-require_relative "serializers/api/v1/detailed/post_serializer"
-require_relative "serializers/api/v1/detailed/user_serializer"
-require_relative "serializers/money_serializer"
-require_relative "serializers/product_serializer"
+loader = Zeitwerk::Loader.new
+loader.push_dir(__dir__)
+loader.collapse("#{__dir__}/*")
+loader.ignore(__FILE__)
+loader.setup
