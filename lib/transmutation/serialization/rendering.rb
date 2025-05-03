fe55f9a68dcec0c0e2194def31a23f6a4482ae02
Daniel Dye <dnitemaeric@gmail.com>
2025-05-03 14:39:45 +0100

    Add docs to the Serialization::Rendering#render method

--- lib/transmutation/serialization/rendering.rb
+++ lib/transmutation/serialization/rendering.rb
@@ -3,6 +3,7 @@
 module Transmutation
   module Serialization
     module Rendering
+      # Serializes the value of the `json` parameter before calling the existing render method.
       def render(json: nil, serialize: true, namespace: nil, serializer: nil, max_depth: 1, **args)
         return super(**args) unless json
         return super(**args, json:) unless serialize
